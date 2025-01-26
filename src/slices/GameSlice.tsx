fc3b7455286061cfdb3b28a042327917c98c7ee7
yashu1412 <135001156+yashu1412@users.noreply.github.com>
2025-01-26 16:28:33 +0530

    fix error of final score

--- src/slices/GameSlice.tsx
+++ src/slices/GameSlice.tsx
@@ -18,17 +18,21 @@ const gameSlice = createSlice({
   reducers: {
     incrementScore(state, action: PayloadAction<number>) {
       state.score += action.payload;
+      console.log('Redux Score Updated:', state.score);
     },
     incrementCorrectAnswers(state) {
       state.correctAnswers += 1;
+      console.log('Correct Answers:', state.correctAnswers);
     },
     incrementWrongAnswers(state) {
       state.wrongAnswers += 1;
+      console.log('Wrong Answers:', state.wrongAnswers);
     },
     resetGame(state) {
       state.score = 0;
       state.correctAnswers = 0;
       state.wrongAnswers = 0;
+      console.log('Game Reset - Score:', state.score);
     },
   },
 });
