2d633470460904fa55399031db4f8810541d4525
yashu1412 <135001156+yashu1412@users.noreply.github.com>
2025-01-26 16:26:57 +0530

    fix error of final score

--- src/containers/GameOverContainer/index.tsx
+++ src/containers/GameOverContainer/index.tsx
@@ -13,17 +13,44 @@ interface RootState {
 const GameOverContainer: React.FC = () => {
   const [isScoreVisible, setIsScoreVisible] = useState(false);
 
-  const finalScore = useSelector((state: RootState) => state.game.score);
+  const finalScore = useSelector((state: RootState) => {
+    console.log('Final Score from Redux:', state.game?.score ?? 'undefined');
+    return state.game?.score ?? 0;
+  });
+
   const correctAnswers = useSelector((state: RootState) => state.game.correctAnswers);
   const wrongAnswers = useSelector((state: RootState) => state.game.wrongAnswers);
 
   useEffect(() => {
+    console.log('=== FINAL SCORE DETAILS ===');
+    console.log('Score:', finalScore);
+    console.log('isScoreVisible:', isScoreVisible);
+  }, [finalScore, isScoreVisible]);
+
+  useEffect(() => {
+    console.log('Final Game Stats:', {
+      finalScore,
+      correctAnswers,
+      wrongAnswers,
+      isScoreVisible
+    });
+
     const timer = setTimeout(() => {
       setIsScoreVisible(true);
+      console.log('Score visibility changed to:', true);
     }, 5000);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [finalScore, correctAnswers, wrongAnswers]);
+
+  // Add another useEffect to log state changes
+  useEffect(() => {
+    console.log('Redux State Updated:', {
+      score: finalScore,
+      correct: correctAnswers,
+      wrong: wrongAnswers
+    });
+  }, [finalScore, correctAnswers, wrongAnswers]);
 
   return (
     <GameOverView
